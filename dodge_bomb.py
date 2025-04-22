677b8fd57b1d396af1e05d2c79eb49c9d8567f63
yuto1106 <c0a2316456@edu.teu.ac.jp>
2025-04-22 17:34:26 +0900

    ex01

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -2,6 +2,7 @@ import os
 import sys
 import pygame as pg
 import random
+import time
 
 
 WIDTH, HEIGHT = 1100, 650
@@ -14,8 +15,6 @@ DELTA = {
 }
 
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
-
-
 
 def check_bound(rct: pg.Rect) -> tuple[bool, bool]:
 
@@ -31,7 +30,8 @@ def check_bound(rct: pg.Rect) -> tuple[bool, bool]:
     if rct.top < 0 or HEIGHT < rct.bottom:
         vert = False
 
-    return besi, vert
+    return besi, vert   
+
 
 
 
@@ -43,11 +43,26 @@ def main():
     kk_rct = kk_img.get_rect()
     kk_rct.center = 300, 200
 
+    kt_sad_img = pg.image.load("fig/8.png")
+    kt_sad_img = pg.transform.rotozoom(kt_sad_img, 0, 0.9)
+
+
     bb_img = pg.Surface((20, 20))
     pg.draw.circle(bb_img, (255, 0, 0), (10, 10), 10)
     bb_rct = bb_img.get_rect()
     bb_rct.center = random.randint(0, WIDTH), random.randint(0, HEIGHT)
     bb_img.set_colorkey((0, 0, 0))
+
+    speed = 1
+    max_speed = 10
+    timer = 300
+
+    c_img = pg.Surface((20, 20))
+    pg.draw.circle(c_img, (255, 0, 0), (10, 10), 10)
+    c_rct = c_img.get_rect()
+    c_rct.center = random.randint(0, WIDTH), random.randint(0, HEIGHT)
+    c_img.set_colorkey((0, 0, 0))
+
     vx, vy = +5, +5
 
     clock = pg.time.Clock()
@@ -59,6 +74,22 @@ def main():
         screen.blit(bg_img, [0, 0]) 
 
         if kk_rct.colliderect(bb_rct):
+            screen.fill((0,0,0))
+
+            fonto = pg.font.Font(None, 60)
+            txt = fonto.render("GAME OVER", True, (255, 255, 255))
+            screen.blit(txt, [WIDTH // 2 - 120 , HEIGHT // 2 ])
+
+            le_sad_rect = kt_sad_img.get_rect()
+            le_sad_rect.center = 20, HEIGHT//2
+
+            ri_sad_rect = kt_sad_img.get_rect()
+            ri_sad_rect.center = 50, HEIGHT//2
+            
+            bb_rct.center = WIDTH // 2, HEIGHT // 2
+
+            pg.display.update()
+            time.sleep(5)
             return
 
         key_lst = pg.key.get_pressed()
@@ -78,9 +109,6 @@ def main():
         #if key_lst[pg.K_RIGHT]:
         #    sum_mv[0] += 5
 
-        
-        
-
         kk_rct.move_ip(sum_mv)
         if check_bound(kk_rct) != (True, True):
             kk_rct.move_ip(-sum_mv[0], -sum_mv[1])
@@ -97,9 +125,47 @@ def main():
 
 
         screen.blit(bb_img, bb_rct)
+        # screen.blit(bb_img, bb_rct)
+        screen.blit(c_img, c_rct)
         pg.display.update()
         tmr += 1
         clock.tick(50)
+
+        if tmr % timer == 0 and speed < max_speed:
+            speed += 1
+            vx = 5 * speed if vx > 0 else -5 * speed
+            vy = 5 * speed if vy > 0 else -5 * speed
+
+        dx = kk_rct.centerx - c_rct.centerx
+        dy = kk_rct.centery - c_rct.centery
+        distance = max((dx**2 + dy**2) ** 0.5, 1) 
+        speed = 3
+
+        c_rct.move_ip(speed * dx / distance, speed * dy / distance)
+
+        if kk_rct.colliderect(bb_rct) or kk_rct.colliderect(c_rct):
+            screen.fill((0,0,0))
+
+            fonto = pg.font.Font(None, 60)
+            txt = fonto.render("GAME OVER", True, (255, 255, 255))
+            screen.blit(txt, [WIDTH // 2 - 120 , HEIGHT // 2 ])
+
+            le_sad_rect = kt_sad_img.get_rect()
+            le_sad_rect.center = 20, HEIGHT//2
+
+            ri_sad_rect = kt_sad_img.get_rect()
+            ri_sad_rect.center = 50, HEIGHT//2
+            
+            bb_rct.center = WIDTH // 2, HEIGHT // 2
+
+            pg.display.update()
+            time.sleep(5)
+            return
+
+        
+
+
+
 
 
 if __name__ == "__main__":
