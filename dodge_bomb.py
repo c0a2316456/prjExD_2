55e5c9531ef88b3736592b7b2a9689d6fd50b970
yuto1106 <c0a2316456@edu.teu.ac.jp>
2025-04-22 14:24:47 +0900

    lec02 alt

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -29,6 +29,7 @@ def main():
     bb_rct = bb_img.get_rect()
     bb_rct.center = random.randint(0, WIDTH), random.randint(0, HEIGHT)
     bb_img.set_colorkey((0, 0, 0))
+    vx, vy = (5,5)
 
     clock = pg.time.Clock()
     tmr = 0
@@ -60,6 +61,7 @@ def main():
 
         kk_rct.move_ip(sum_mv)
         screen.blit(kk_img, kk_rct)
+        bb_rct.move_ip(vx, vy)
         screen.blit(bb_img, bb_rct)
         pg.display.update()
         tmr += 1
